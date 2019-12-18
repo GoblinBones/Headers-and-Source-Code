19f5bc3a971cbdf51c67cbf31bebef9fb7d2ce92
LivingBones <luigarc2@uat.edu>
2019-12-17 20:27:22 -0700

    Add project files.

---
+++ Headers and Source Code/Header.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void DisplayIntroduction();
+void DisplayCurrentArea();
+void DisplayBattle();
+string InTakeStringsToProcess(string promptForUser);
+string IntakeNumberToProcess(string promptForUser);
---
+++ Headers and Source Code/Source.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include "Header.h"
+
+using namespace std;
+
+
+int main()
+{
+	DisplayIntroduction();
+	DisplayCurrentArea();
+	DisplayBattle();
+
+
+	system("pause");
+}
+
